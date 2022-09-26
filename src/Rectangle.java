505a73e7a0f8b2104fcc052536fb1e475a5700da
tenzin <tdukdak9287@bths.edu>
2022-09-25 22:00:28 -0400

    I finished playing with the dimensions and functions of the class and completed the fencing questions I also finished creating a perimeter method

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,10 +1,14 @@
-public class Rectangle
-{
+import org.w3c.dom.css.Rect;
+
+public class Rectangle {
     /* Below are the Rectangle class "instance variables" (or "fields");
         these are the attributes (data) saved in each Rectangle object
     */
     private int length;
     private int width;
+
+
+
 
    /* Below are the Rectangle class "constructor" methods;
        this class has THREE constructors! (we will learn later about
@@ -12,22 +16,19 @@ public class Rectangle
    */
 
     // This is the constructor to create a Rectangle object with a particular width and length
-    public Rectangle(int rectLength, int rectWidth)
-    {
+    public Rectangle(int rectLength, int rectWidth) {
         length = rectLength;
         width = rectWidth;
     }
 
     //This is the constructor to create a Rectangle object with equal width and length (a square)
-    public Rectangle(int side)
-    {
+    public Rectangle(int side) {
         length = side;
         width = side;
     }
 
     // This is the "no-argument constructor" which creates a Rectangle object with default values
-    public Rectangle()
-    {
+    public Rectangle() {
         length = 100;
         width = 50;
     }
@@ -40,39 +41,39 @@ public class Rectangle
    */
 
     // Returns the length of the Rectangle (a "getter" method)
-    public int getLength()
-    {
+    public int getLength() {
         return length;
     }
 
     // Sets the length of the Rectangle to newLength ("setter" method)
-    public void setLength(int newLength)
-    {
+    public void setLength(int newLength) {
         length = newLength;
     }
 
     // Returns the width of the Rectangle ("getter" method)
-    public int getWidth()
-    {
+    public int getWidth() {
         return width;
     }
 
     // Sets the width of the Rectangle to newWidth ("setter" method)
-    public void setWidth(int newWidth)
-    {
+    public void setWidth(int newWidth) {
         width = newWidth;
     }
 
     // Calculates and returns the area of the Rectangle
-    public int calculateArea()
-    {
+    public int calculateArea() {
         int area = width * length;
         return area;
     }
 
+    public int calculatePerimeter() {
+        int perimeter = (2 * width) + (2 * length);
+        return perimeter;
+    }
+
+
     // Prints the Rectangle's dimensions
-    public void printRectangleInfo()
-    {
+    public void printRectangleInfo() {
         System.out.println("This rectangle's length is " + length + " and width is " + width + ".");
         System.out.println("It has an area of " + calculateArea());
     }
--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -1,11 +1,20 @@
 public class U2L2Runner {
     public static void main(String[] args) {
         Rectangle rect1 = new Rectangle(150, 200);
+        rect1.setWidth(125);
         rect1.printRectangleInfo();
         Rectangle rect2 = new Rectangle(100);
+        rect2.setWidth(125);
+        rect2.setLength(125);
         rect2.printRectangleInfo();
         Rectangle rect3 = new Rectangle();
+        rect3.setWidth(125);
         rect3.printRectangleInfo();
+        System.out.println("you will need " + (rect1.calculateArea() + rect2.calculateArea() + rect3.calculateArea()) + " sq ft of fencing");
+        System.out.println("your total fence perimeter will be " + (rect1.calculatePerimeter() + rect2.calculatePerimeter() + rect3.calculatePerimeter()) + " sq ft");
+
+
+
 
 
     }
